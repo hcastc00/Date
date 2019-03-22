57617a87e286376308171c2dd20d3ede71c17bb9
hcastc00 <hcastc00@estudiantes.unileon.es>
2019-03-23 00:56:41 +0100

    Añadidos métodos hasta el "adivinador de fechas"

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -53,7 +53,7 @@ public class Date {
 		this.month = month;
 	}
 	
-	public void setDay(int year){
+	public void setYear(int year){
 		this.year = year;
 	}
 	
@@ -92,49 +92,279 @@ public class Date {
 
 
 	public boolean isSameYearIf(Date date){
-		return =false
 		if (this.getYear() == getYear()) {
-			return = true;
+			return true;
+		}
+		else{
+			return false;
 		}
 	}	
 	
 	public boolean isSameMonthIf(Date date){
-		return =false
 		if (this.getMonth() == getMonth()) {
-			return = true;
+			return  true;
+		}
+		else{
+			return false;
 		}
 	}	
 
-	public boolean isSameYearIf(Date date){
-		return =false
+	public boolean isSameDayIf(Date date){
 		if (this.getDay() == getDay()) {
-			return = true;
+			return  true;
+		}
+		else{
+			return false;
 		}
 	}	
 
 	public boolean isSameIf(Date date){
-		return =false
-		if (this.date == date){
-			return = true;
+		if (isSameDay(date) && isSameMonth(date) && isSameYear(date)){
+			return  true;
+		}
+		else{
+			return false;
 		}
 	}	
 
+	
 	public boolean isSameYear(Date date){
-		return =this.getYear() == getYear();
+		return this.getYear() == getYear();
 	}	
 
 	public boolean isSameMonth(Date date){
-		return =this.getMonth() == getMonth();
+		return this.getMonth() == getMonth();
 	}	
 
 	public boolean isSameDay(Date date){
-		return =this.getDay() == getDay();
+		return this.getDay() == getDay();
 	}	
 
 	public boolean isSame(Date date){
-		return =this.date == date;
+		return (isSameDay(date) && isSameMonth(date) && isSameYear(date));
 	}	
 
+	
+	public String printMonthName(){
+		StringBuffer salida = new StringBuffer();
+		switch(this.month){
+			case 1:
+				salida.append("Enero");
+				break;
+		
+			case 2:
+				salida.append("Febrero");
+				break;
+			
+			case 3:
+				salida.append("Marzo");
+				break;
+				
+			case 4:
+				salida.append("Abril");
+				break;
+		
+			case 5:
+				salida.append("Mayo");
+				break;
+			
+			case 6:
+				salida.append("Junio");
+				break;
+				
+			case 7:
+				salida.append("Julio");
+				break;
+		
+			case 8:
+				salida.append("Agosto");
+				break;
+			
+			case 9:
+				salida.append("Septiembre");
+				break;
+				
+			case 10:
+				salida.append("Octubre");
+				break;
+		
+			case 11:
+				salida.append("Noviembre");
+				break;
+			
+			case 12:
+				salida.append("Diciembre");
+				break;
+		}
+	return salida.toString();
+	}
+
+	public String printSeasonOfMonth(){
+		StringBuffer salida = new StringBuffer("La estacion predominante de este mes es ");
+		
+		switch(this.getMonth()){
+			case 1: //next
+			case 2: //next
+			case 3:
+				salida.append("Invierno");
+				break;
+			
+			case 4: //next
+			case 5: //next
+			case 6:
+				salida.append("Primavera");
+				break;
+				
+			case 7: //next
+			case 8: //next
+			case 9:
+				salida.append("Verano");
+				break;
+				
+			case 10: //next
+			case 11: //next
+			case 12:
+				salida.append("Otoño");
+				break;
+		}
+	return salida.toString();
+	}
+
+
+	public String monthsToEndYear(){
+		int monthsToEndYear = 12 - this.getMonth();
+		StringBuffer salida = new StringBuffer("Hasta finalizar el año queda(n) "+monthsToEndYear+" mes(es)");
+		return salida.toString();
+	}
+
+
+	public String dateToString(){
+		return this.day +" de "+ this.printMonthName() +" de "+ this.year;
+	}
+
+	public String printDatesToEndMonth(){
+		StringBuffer salida = new StringBuffer("Hasta finalizar el mes pasarán los días ");
+		int daysInMonth;
+		switch(month){
+			case 1: //next
+			case 3: //next
+			case 5: //next
+			case 7: //next
+			case 8: //next
+			case 10: //next
+			case 12:
+				daysInMonth = 31;
+				break;
+			
+			case 2:
+				daysInMonth = 28;
+				break;
+			
+			default:
+				daysInMonth = 30;
+		}
+		
+		for (int i = 0;i<=daysInMonth-this.day;i++ ) {
+			salida.append((this.day+i)+"/"+this.month+"/"+this.year+", ");
+		}
+		return salida.toString();
+	}
+
+	public String printMonthsWhithSameDays(){
+		StringBuffer salida = new StringBuffer(this.printMonthName()+" tiene el mismo número de días que ");
+
+		switch(this.getMonth()){
+				case 1:
+					salida.append("Marzo, Mayo, Julio, Agosto, Octubre y Diciembre");
+					break;
+			
+				case 2:
+					salida.append("ningún otro més");
+					break;
+				
+				case 3:
+					salida.append("Enero, Mayo, Julio, Agosto, Octubre y Diciembre");
+					break;
+					
+				case 4:
+					salida.append("Junio, Septiembre y Noviembre");
+					break;
+			
+				case 5:
+					salida.append("Enero, Marzo, Julio, Agosto, Octubre y Diciembre");
+					break;
+				
+				case 6:
+					salida.append("Abril, Septiembre y Noviembre");
+					break;
+					
+				case 7:
+					salida.append("Enero, Marzo, Mayo, Agosto, Octubre y Diciembre");
+					break;
+			
+				case 8:
+					salida.append("Enero, Marzo, Mayo, Junio, Octubre y Diciembre");
+					break;
+				
+				case 9:
+					salida.append("Abril, Junio y Noviembre");
+					break;
+					
+				case 10:
+					salida.append("Enero, Marzo, Mayo, Junio, Agosto y Diciembre");
+					break;
+			
+				case 11:
+					salida.append("Abril, Junio y Septiembre");
+					break;
+				
+				case 12:
+					salida.append("Enero, Marzo, Mayo, Junio, Agosto y Octubre");
+					break;
+		}
+		return salida.toString();
+	}
+
+
+	public int daysSinceYearBeginning(){
+		int daysInMonth, daysSinceYearBeginning = 0;
+		for (int i=1;i<this.month;i++ ) {
+			switch(i){
+				case 1: //next
+				case 3: //next
+				case 5: //next
+				case 7: //next
+				case 8: //next
+				case 10: //next
+				case 12:
+					daysInMonth = 31;
+					break;
+				
+				case 2:
+					daysInMonth = 28;
+					break;
+				
+				default:
+					daysInMonth = 30;
+			}
+			daysSinceYearBeginning = daysSinceYearBeginning + daysInMonth;
+		}
+		daysSinceYearBeginning = daysSinceYearBeginning + this.day;
+
+		return daysSinceYearBeginning;
+	}
+
+
+	//esta mal, revisar
+	public int attemptsToGuessDate(){
+		int randomDay = 0, randomMonth = 0, intentos = 0;
+		while(this.day!=randomDay && this.month!=randomMonth){
+			randomDay = (int)(Math.random() * ((31 - 1) + 1)) + 1;
+			randomMonth = (int)(Math.random() * ((12 - 1) + 1)) + 1;
+			intentos++;
+		}
+		return intentos;
+	}
 
 
 
--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -7,11 +7,20 @@ public class MainDate {
 		Date today;
 
 		try {
-			today = new Date(99, 23, 2017);
+			today = new Date(12, 04, 2017);
 			System.out.println(today.toString());
+			System.out.println(today.printMonthName());
+			System.out.println(today.printSeasonOfMonth());
+			System.out.println(today.monthsToEndYear());
+			System.out.println(today.dateToString());
+			System.out.println(today.printDatesToEndMonth());
+			System.out.println(today.printMonthsWhithSameDays());
+			System.out.println("Desde el inicio del año han pasado "+today.daysSinceYearBeginning());
+			System.out.println("Se han realizado "+today.attemptsToGuessDate()+" intentos para acertar la fecha.");
 		} catch (DateException e) {
 			System.out.println(e.getMessage());
 		}
+
 
 	}
 
